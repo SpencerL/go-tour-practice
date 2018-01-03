3dc251a0ee9a075b1473f8e48e4c32f94b2de72d
spencerL <youngth@hotmail.com>
2018-01-03 22:10:24 +0800

    init commit

---
+++ slice_practice/slice_practice.go
@@ -0,0 +1,24 @@
+package main
+// 练习 slice: https://tour.go-zh.org/moretypes/15
+
+import (
+	"fmt"
+	 "golang.org/x/tour/pic"
+	)
+func Pic(dx, dy int) [][]uint8 {
+	a := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		a[i] = make([]uint8, dx)
+	}
+	
+	for i := 0; i < dy; i++ {
+		for j := 0; j < dx; j++ {
+			a[i][j] = uint8((i+j) / 2)
+		}
+	}
+	return a	
+}
+
+func main() {
+	pic.Show(Pic)
+}
